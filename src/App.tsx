baf678d4d550e8c7c7ae71bb9c061493b21c9b90
Elvis Taco <etaco@gruporoi.pe>
2022-08-09 11:16:43 -0500

    inicinado proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count iss {count}
         </button>
 
         <Button onClick={() => setCount((count) => count + 1)} variant="contained"> count is {count} </Button>
